8b6811429dc413e9d1fdaf8b43fb5eddda53aa4d
ming123jew <ming123jew123@qq.com>
2018-06-11 23:00:22 +0800

    v1.0.0

---
+++ vendor/tmtbe/swooledistributed/src/Server/Components/AOP/Proxy.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangjincheng
+ * Date: 17-10-12
+ * Time: 下午7:17
+ */
+
+namespace Server\Components\AOP;
+
+
+abstract class Proxy implements IProxy
+{
+    /**
+     * @var mixed
+     */
+    protected $own;
+
+    public function __construct($own)
+    {
+        $this->own = $own;
+    }
+
+    public abstract function beforeCall($name, $arguments = null);
+
+    public abstract function afterCall($name, $arguments = null);
+
+    public function __call($name, $arguments)
+    {
+        $this->beforeCall($name, $arguments);
+        try {
+            $result = sd_call_user_func_array([$this->own, $name], $arguments);
+            return $result;
+        }catch (\Throwable $e){
+            throw  $e;
+        }finally{
+            $this->afterCall($name, $arguments);
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        $this->own->$name = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->own->$name;
+    }
+
+    public function getOwn()
+    {
+        return $this->own;
+    }
+}
